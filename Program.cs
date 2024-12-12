232b1fd7d138c97c10752cd54ab91c3b3144e576
AKevra <61617251+AKevra@users.noreply.github.com>
2024-12-12 23:26:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Hometask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your full name:");
+            var userName = Console.ReadLine();
+            Console.WriteLine("Enter your birthday in format (dd.mm.yyyy):");
+            var birthDay = Console.ReadLine();
+            Console.WriteLine($"Your name: {userName}, your birthday: {birthDay}");
+        }
+    }
+}
